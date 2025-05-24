10f88f95f8126ac28590981f9370b01c52a063c6
Ahmed Lemssiah <art3que@gmail.com>
2025-05-24 07:49:24 +0200

    Added 404, moved the navCats Loop to an external componant

--- _components/Assets/Nav.tsx
+++ _components/Assets/Nav.tsx
@@ -1,27 +1,12 @@
 "use client";
 import Classes from "./Nav.module.scss";
-import { usePathname } from "next/navigation";
-import { useEffect, useState } from "react";
-import { Cat } from "@/types/cat";
 import Link from "next/link";
-import Image from "next/image";
+import NavCats from "./NavCats";
+import { usePathname } from "next/navigation";
 
 const Nav = () => {
-  const [loading, setLoading] = useState(true);
-  const [cats, setCats] = useState<Cat[]>([]);
 
   const pathname = usePathname();
-
-  useEffect(() => {
-    fetch("/api/cats")
-      .then((res) => res.json())
-      .then((json) => {
-        setCats(json.data as Cat[]);
-      })
-      .catch((err) => console.error(err))
-      .finally(() => setLoading(false));
-  }, []);
-
   
   return (
     <>
@@ -30,9 +15,7 @@ const Nav = () => {
         lg:opacity-100 lg:relative lg:top-0 lg:bg-transparent lg:[pointer-events:auto_!important]
         `}
       >
-        {loading ? (
-          <p className="text-center text-light-100/25">Loading...</p>
-        ) : (
+       
           <ul className="mainNav__Items lg:flex lg:gap-[1rem] ">
             <li className="mainNav__item hidden lg:block">
               <Link
@@ -44,29 +27,9 @@ const Nav = () => {
                 Home
               </Link>
             </li>
-            {cats.map(({ id, path, name, img }) => (
-              <li key={id} className={`mainNav__item`}>
-                <Link
-                  href={path}
-                  className={`${
-                    pathname === path ? "active" : ""
-                  } text-center  w-full text-[13px] leading-[25px] tracking-[2px] font-bold block p-1 uppercase lg:p-0 transition-all duration-[0.3s] lg:text-light-100/50 lg:hover:text-light-100 `}
-                >
-                  <span className="icon lg:hidden">
-                    <Image
-                      src={`/shared/desktop/${img.src}`}
-                      alt={name}
-                      width={img.width}
-                      height={img.height}
-                      className="block mx-auto"
-                    />
-                  </span>
-                  <span>{name}</span>
-                </Link>
-              </li>
-            ))}
+           <NavCats />
           </ul>
-        )}
+       
       </nav>
     </>
   );
---
+++ app/headphones/page.tsx
@@ -0,0 +1,10 @@
+const page = () => {
+  return (
+    <div>
+      <h1>Headphones</h1>
+      <p>Headphones are a pair of small loudspeakers that are designed to be held close to a user's ears.</p>
+    </div>
+  )
+}
+
+export default page
